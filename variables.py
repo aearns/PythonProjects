900fcd98ee2c024a5d4c3e78e717140fb89db73f
aearns <aearns@gmail.com>
2022-05-04 15:30:44 +0000

    Updates

--- variables.py
+++ variables.py
@@ -5,14 +5,28 @@ first_var = 3
 string_variable = "hello"
 single_quote_var = 'string with single quote'
 
-print(a + b)
-print(string_variable)
-print("Hello there!")
-print(456)
+#print(a + b)
+#print(string_variable)
+#print("Hello there!")
+#print(456)
 
 var1 = "Hey"
 var2 = "Hey"
 
 num1 = 6
 num2 = 2
+
+##
+
+def my_print_method(my_parameter):
+    print(my_parameter)
+
+my_print_method("Hello, world!")
+
+def my_multiply_method(Numb1, Numb2, Var):
+    return Numb1 / Numb2 + 10
+
+result = my_multiply_method(8, 9, 1)
+my_print_method(result)
+
 
