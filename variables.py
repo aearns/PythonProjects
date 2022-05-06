e620a9a3a4c8dd571bcb81841c545e17aaa7c45e
aearns <aearns@gmail.com>
2022-05-06 17:04:10 +0000

    Continuation

--- variables.py
+++ variables.py
@@ -11,7 +11,7 @@ single_quote_var = 'string with single quote'
 
 #print(a + b)
 #print(string_variable)
-#print("Hello there!")
+#print("Hello [there]!")
 #print(456)
 
 var1 = "Hey"
@@ -20,11 +20,12 @@ var2 = "Hey"
 num1 = 6
 num2 = 2
 
-##
+### Def function
+ 
+land_animals = ["man", "dog", "cat", "goat", "sheep", "lion"]
 
 def my_print_method(my_parameter):
     print(my_parameter)
-
 my_print_method("Hello, world!")
 
 def my_multiply_method(Numb1, Numb2, Var):
@@ -33,4 +34,14 @@ def my_multiply_method(Numb1, Numb2, Var):
 result = my_multiply_method(8, 9, 1)
 my_print_method(result)
 
+def animals(land_animals):
+    print(land_animals)
+animals
 
+#def animals_in_water(frog, fish, turtle):
+#    print(turtle)
+#animals_in_water(1, 3, 5)
+
+def f(y):
+    return y**2 + 1
+f(4)
